2be97c49556647409e37bf821bd5a9a7391eec9a
Gwilymm <marin.davies@gmail.com>
2022-05-25 16:31:20 +0200

    whaouh

--- src/Entity/Ticket.php
+++ src/Entity/Ticket.php
@@ -10,6 +10,22 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class Ticket
 {
+
+    private $currentPlace;
+    private $title;
+    private $content;
+
+    // getter/setter methods must exist for property access by the marking store
+    public function getCurrentPlace()
+    {
+        return $this->currentPlace;
+    }
+
+    public function setCurrentPlace($currentPlace, $context = [])
+    {
+        $this->currentPlace = $currentPlace;
+    }
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
