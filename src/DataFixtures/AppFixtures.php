a22b53a03e39e37dd543d973ca15a39925fd0842
Gwilymm <marin.davies@gmail.com>
2022-05-25 12:03:30 +0200

    a table

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,20 +2,56 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Departement;
-use App\Entity\Ticket;
-use App\Faker\Provider\ImmutableDateTime;
 use Faker\Factory;
+use App\Entity\User;
+use App\Entity\Ticket;
+use App\Entity\Departement;
 use Doctrine\Persistence\ObjectManager;
+use App\Faker\Provider\ImmutableDateTime;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    protected UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
 
 
         $faker = Factory::create('fr_FR');
+
+
+        /**
+         * création de 5 utilisateurs
+         */
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User;
+
+            $hash = $this->hasher->hashPassword($user, "afpa");
+
+            if ($u === 0) {
+                $user->setRoles(["ROLE_ADMIN"])
+                    ->setEmail("admin@test.bzh");
+            } else {
+                $user->setEmail("user{$u}@test.bzh");
+            }
+
+
+
+            $user->setName($faker->name())
+                ->setPassword($hash);
+
+            /**
+             * persistence des données
+             */
+            $manager->persist($user);
+        }
 
         /**
          * création de département *10
