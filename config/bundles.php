d8df5fc9dcc8f9d5c74ca17d74313a3ad9a8aa8b
Gwilymm <marin.davies@gmail.com>
2022-06-01 16:42:11 +0200

    recup mdp

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
